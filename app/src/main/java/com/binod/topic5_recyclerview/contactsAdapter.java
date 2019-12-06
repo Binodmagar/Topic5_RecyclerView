b729e3543620e76ad61905b1fdb84c143cee3924
Binodmagar <ranabinod615@gmail.com>
2019-12-06 14:49:08 +0545

    contact adapter done and extend adapter, list contacts

--- app/src/main/java/com/binod/topic5_recyclerview/contactsAdapter.java
+++ app/src/main/java/com/binod/topic5_recyclerview/contactsAdapter.java
@@ -1,13 +1,44 @@
 package com.binod.topic5_recyclerview;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class contactsAdapter {
+import java.util.List;
+
+public class contactsAdapter extends RecyclerView.Adapter<contactsAdapter.ContactsViewHolder> {
+
+    Context context;//to get name of activities that java class get data from activity
+
+    List<Contacts> contactsList;//to get list of contact in array
+
+    public contactsAdapter(Context context, List<Contacts> contactsList) {
+        this.context = context;
+        this.contactsList = contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+        //attach in design
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+        //attach data in design by one by one
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+        //count number of list or data shown
+    }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder {
 
