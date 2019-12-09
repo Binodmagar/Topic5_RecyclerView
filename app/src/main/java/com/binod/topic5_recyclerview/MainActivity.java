44fa44bdcfb37dc8786f099650ec99d054ab0ed6
Binodmagar <ranabinod615@gmail.com>
2019-12-09 13:59:01 +0545

    layout_contact.xml file finished in linear layout and contact_laypit.xml file done in constraint layout

--- app/src/main/java/com/binod/topic5_recyclerview/MainActivity.java
+++ app/src/main/java/com/binod/topic5_recyclerview/MainActivity.java
@@ -23,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
 
         //create list of contact to display in recyclerview
         List<Contacts> contactsList = new ArrayList<>();
-        contactsList.add(new Contacts("Robert","9867543089", R.drawable.logo));
-        contactsList.add(new Contacts("John", "9846530001", R.drawable.logo));
-        contactsList.add(new Contacts("Roman","9867517650", R.drawable.logo));
+        contactsList.add(new Contacts("Robert","9867543089", R.drawable.john));
+        contactsList.add(new Contacts("John", "9846530001", R.drawable.roman));
+        contactsList.add(new Contacts("Roman","9867517650", R.drawable.robert));
 
         contactsAdapter contactsAdapter = new contactsAdapter(this, contactsList);
         recyclerView.setAdapter(contactsAdapter);
